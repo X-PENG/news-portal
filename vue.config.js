fb462bf0e98a05894cc33beef2cdeb8b6e04535e
PENG <2318511681@qq.com>
2021-04-24 23:42:48 +0800

    加上页面titl

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = '新闻网' // page title
+const name = '家里蹲大学新闻网' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
