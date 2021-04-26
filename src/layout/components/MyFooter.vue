a23ab27ffa280298742a52d900d24d176f56af4c
PENG <2318511681@qq.com>
2021-04-26 22:12:28 +0800

    完成footer

--- src/layout/components/MyFooter.vue
+++ src/layout/components/MyFooter.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="app-footer">
         <div class="app-footer-content">
-            <h1 style="text-align: center">Footer</h1>
+            <!-- <h1 style="text-align: center">Footer</h1> -->
+            <span>关于我们</span>
+            <i>|</i>
+            <span>投稿须知</span>
+            <i>|</i>
+            <span>投稿邮箱：xxxxxxxxxx@163.com</span>
         </div>
         <div class="copy-right">
             <span class="copy-right-symbol">©</span>2021 江西财经大学 谢志朋 版权所有
@@ -24,7 +29,7 @@
 @import '~@/styles/variables.scss';
 
 .app-footer {
-    height: 160px;
+    height: 140px;
     background-color: $topic-color;
     color: #FFFFFF;
 
@@ -33,7 +38,29 @@
     }
 
     .app-footer-content {
-        height: 100px;
+        height: 80px;
+        font-family: 'Microsoft YaHei',SimSun,SimHei ,"STHeiti Light",STHeiti,"Lucida Grande", Tahoma,Arial, Helvetica, sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+
+        span {
+            color: #fff;
+            font-style: normal;
+            font-size: 14px;
+            line-height: 28px;
+            display: inline-block;
+        }
+
+        i {
+            color: #fff;
+            font-style: normal;
+            font-size: 14px;
+            line-height: 28px;
+            display: inline-block;
+            margin: 0px 15px;
+        }
     }
 
     .copy-right {
